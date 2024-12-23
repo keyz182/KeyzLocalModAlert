0905c2a696ee6eeeaa5247c94cd2ce4a78b5519f
Kieran David Evans <keyz182@gmail.com>
2024-12-23 11:31:10 +0000

    Cookie cutter initial commit

---
+++ 1.5/Source/KeyzLocalModAlert/KeyzLocalModAlertMod.cs
@@ -0,0 +1,19 @@
+﻿using Verse;
+using UnityEngine;
+using HarmonyLib;
+
+namespace KeyzLocalModAlert;
+
+public class KeyzLocalModAlertMod : Mod
+{
+
+    public KeyzLocalModAlertMod(ModContentPack content) : base(content)
+    {
+        Log.Message("Hello world from KeyzLocalModAlert");
+#if DEBUG
+        Harmony.DEBUG = true;
+#endif
+        Harmony harmony = new Harmony("keyz182.rimworld.KeyzLocalModAlert.main");	
+        harmony.PatchAll();
+    }
+}
